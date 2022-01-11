84f6b1e181b5b088919dd7942fa9e5064879a680
lucciii33 <angelomaiele@gmail.com>
2022-01-11 18:27:27 +0000

    Domain Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let pronoun = ["the", "our", "his"];
+  let adj = ["great", "big", "cool"];
+  let noun = ["jogger", "racoon", "blog"];
+  let hacks = [".com", ".net", ".uk"];
+
+  //let finalDomainGenerator = [];
+
+  for (let pronoun of pronoun) {
+    for (let adj of adj) {
+      for (let noun of noun) {
+        for (let hacks of hacks) {
+          document.querySelector(
+            "#domains"
+          ).innerHTML += `<ul>${pronoun}${adj}${noun}${hacks}</ul>`;
+          //console.log("<ul>" + pronoun + adj + noun + hacks + "</ul>");
+        }
+      }
+    }
+  }
 };
